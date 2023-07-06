6a1120baaca8a0a460fb773c7cb04b15cb43eff0
Kira <kira.kaur.sidhu@gmail.com>
2023-07-06 15:18:12 -0700

    Added checks to end game and created loop to play game until it ends

--- board.py
+++ board.py
@@ -22,30 +22,65 @@ engine = chess.engine.SimpleEngine.popen_uci("stockfish")
 move_list = []
 engine_list = []
 
-for _ in range(5):
-    new_move = input("Input your move: " )
 
-    move_list.append(new_move)
+def check_if_game_ends(board):
+    if board.is_checkmate():
+        print("Checkmate")
+        return True
+    if board.is_stalemate():
+        print("Stalemate")
+        return True
+    if board.is_insufficient_material():
+        print("Insufficient Material")
+        return True
+    if board.can_claim_draw():
+        print("Can Claim Draw")
+        return True
+    if board.can_claim_threefold_repetition():
+        print("Can Claim Threefold Repetition")
+        return True
+    if board.can_claim_fifty_moves():
+        print("Can Claim Fifty Moves")
+        return True
+    if board.is_fivefold_repetition():
+        print("Is Fivefold Repetition")
+        return True
+    if board.is_seventyfive_moves():
+        print("Completed 75 Moves")
+        return True
+    return False
 
+
+while True:
     game_board = display.start(board.fen())
-    while not display.check_for_quit():
-        if move_list:
-            try:
-                board.push_san(move_list.pop(0))
-            except chess.IllegalMoveError:
-                print("Illegal Move Error!")
-                break
-            display.update(board.fen(), game_board)
-            result = engine.play(board, chess.engine.Limit(time=2.0))
-            engine_list.append(result)
-            board.push(result.move)
-            display.update(board.fen(), game_board)
-            if board.is_checkmate():
-                print("Checkmate")
-            elif board.is_check():
-                print("Check")
-        sleep(1)
+    new_move = input("Input your move: " )
+    move_list.append(new_move)
+    
+    try:
+        board.push_san(new_move)
+        display.update(board.fen(), game_board)
+    except chess.IllegalMoveError:
+        print("Illegal Move Error!")
+        continue
+
+    if check_if_game_ends(board):
         break
+    elif board.is_check():
+        print("Check")
+
+    sleep(1)
+    result = engine.play(board, chess.engine.Limit(time=2.0))
+    engine_list.append(result)
+    board.push(result.move)
+    display.update(board.fen(), game_board)
+    
+
+    if check_if_game_ends(board):
+        break
+    elif board.is_check():
+        print("Check")
+
+    sleep(1)
 
 
 engine.quit()
