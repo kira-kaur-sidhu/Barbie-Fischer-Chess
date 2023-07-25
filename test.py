8d799fc80f149770b0429d11b03f075aef7f4842
Kimberly Lantigua <klantigua11@gmail.com>
2023-07-25 15:57:19 -0700

    Replaced stockfish engine with our own engine! Engine is optimal under conditions where the opening is played, the engine takes over after the opening and plays into the end game.

--- test.py
+++ test.py
@@ -8,6 +8,8 @@ import chess.pgn
 import chess.polyglot
 # endgame tablebase
 import chess.syzygy
+#python's engine 
+import chess.engine
 # our engine
 from our_engine import ourEngine
 # Initalize chessboard display
@@ -16,146 +18,171 @@ from time import sleep
 from openings import opening_table
 
 
-# def check_if_game_ends(board):
-#     if board.is_checkmate():
-#         print("Checkmate")
-#         return True
-#     if board.is_stalemate():
-#         print("Stalemate")
-#         return True
-#     if board.is_insufficient_material():
-#         print("Insufficient Material")
-#         return True
-#     if board.can_claim_draw():
-#         print("Can Claim Draw")
-#         return True
-#     if board.can_claim_threefold_repetition():
-#         print("Can Claim Threefold Repetition")
-#         return True
-#     if board.can_claim_fifty_moves():
-#         print("Can Claim Fifty Moves")
-#         return True
-#     if board.is_fivefold_repetition():
-#         print("Is Fivefold Repetition")
-#         return True
-#     if board.is_seventyfive_moves():
-#         print("Completed 75 Moves")
-#         return True
-#     return False
+def check_if_game_ends(board):
+    if board.is_checkmate():
+        print("Checkmate")
+        return True
+    if board.is_stalemate():
+        print("Stalemate")
+        return True
+    if board.is_insufficient_material():
+        print("Insufficient Material")
+        return True
+    if board.can_claim_draw():
+        print("Can Claim Draw")
+        return True
+    if board.can_claim_threefold_repetition():
+        print("Can Claim Threefold Repetition")
+        return True
+    if board.can_claim_fifty_moves():
+        print("Can Claim Fifty Moves")
+        return True
+    if board.is_fivefold_repetition():
+        print("Is Fivefold Repetition")
+        return True
+    if board.is_seventyfive_moves():
+        print("Completed 75 Moves")
+        return True
+    return False
 
-# def user_moves(board, game_board, move_list):
-#     new_move = input("Input your move: " )
+def user_moves(board, game_board, move_list):
+    new_move = input("Input your move: " )
     
-#     try:
-#         board.push_san(new_move)
-#         move_list.append(new_move)
-#         display.update(board.fen(), game_board)
-#     except chess.IllegalMoveError:
-#         print("Illegal Move Error!")
-#         user_moves(board, game_board, move_list)
+    try:
+        board.push_san(new_move)
+        move_list.append(new_move)
+        display.update(board.fen(), game_board)
+    except chess.IllegalMoveError:
+        print("Illegal Move Error!")
+        user_moves(board, game_board, move_list)
         
-#     sleep(1)
-#     return
+    sleep(1)
+    return
 
-# def engine_moves(board, game_board, engine_list, engine):
-#     engine = ourEngine(board, "white")
-#     engine.evaluation("white")
+def engine_moves(board, game_board, engine_list, engine):
+    engine = ourEngine(board, "white")
+    engine.evaluation("white")
 
-# def opening_moves(board, game_board, engine_list, current_opening, move_list, engine):
-#     move_index = len(engine_list) + 1 
-#     result = None 
+def opening_moves(board, game_board, engine_list, current_opening, move_list, engine):
+    move_index = len(engine_list) + 1 
+    result = None 
 
-#     for line in current_opening["variations"]: 
-#         if not move_list: 
-#             print("Moved")
-#             result = line[move_index][0]
-#             board.push_san(result)
-#             break
-#         elif len(move_list) > 0: 
-#             try:
-#                 if line[move_index-1][1] == move_list[-1] and engine_list[-1] == line[move_index-1][0]:
-#                     try:
-#                         print("Moved")
-#                         result = line[move_index][0]
-#                         board.push_san(result)
-#                         break
-#                     except chess.IllegalMoveError:
-#                         continue
-#                 else: 
-#                     continue
-#             except IndexError:
-#                 continue 
+    for line in current_opening["variations"]: 
+        if not move_list: 
+            print("Moved")
+            result = line[move_index][0]
+            board.push_san(result)
+            break
+        elif len(move_list) > 0: 
+            try:
+                if line[move_index-1][1] == move_list[-1] and engine_list[-1] == line[move_index-1][0]:
+                    try:
+                        print("Moved")
+                        result = line[move_index][0]
+                        board.push_san(result)
+                        break
+                    except chess.IllegalMoveError:
+                        continue
+                else: 
+                    continue
+            except IndexError:
+                continue 
 
-#     if not result: 
-#         result = engine.play(board, chess.engine.Limit(time=2.0))
-#         board.push(result.move)
+    if not result: 
+        result = engine.search(board, 3, "white")
+        board.push(result)
 
-#     engine_list.append(result)
-#     display.update(board.fen(), game_board)
-#     sleep(1)
+    engine_list.append(result)
+    display.update(board.fen(), game_board)
+    sleep(1)
 
 
 
-# def play_chess_game():
-#     user_color = input("Do you want to play as white or black? ")
-#     engine_opening = input("What opening do you want to play against? ")
+def play_chess_game():
+    user_color = input("Do you want to play as white or black? ")
+    engine_opening = input("What opening do you want to play against? ")
 
-#     for item in opening_table:
-#         if engine_opening == item["name"]:
-#             current_opening = item
+    for item in opening_table:
+        if engine_opening == item["name"]:
+            current_opening = item
 
-#     board = chess.Board()
-#     engine = chess.engine.SimpleEngine.popen_uci("stockfish")
-#     move_list = []
-#     engine_list = []
+    board = chess.Board()
+    engine = ourEngine(board, "white", 3)
+    move_list = []
+    engine_list = []
 
-#     while True:
-#         game_board = display.start(board.fen())
+    while True:
+        game_board = display.start(board.fen())
 
-#         if user_color == "white":
-#             user_moves(board, game_board, move_list)
-#             if check_if_game_ends(board):
-#                 winner = "white"
-#                 break
-#             elif board.is_check():
-#                 print("Check")
+        if user_color == "white":
+            user_moves(board, game_board, move_list)
+            if check_if_game_ends(board):
+                winner = "white"
+                break
+            elif board.is_check():
+                print("Check")
 
-#             engine_moves(board, game_board, engine_list, engine)
-#             if check_if_game_ends(board):
-#                 winner = "black"
-#                 break
-#             elif board.is_check():
-#                 print("Check")
+            engine_moves(board, game_board, engine_list, engine)
+            if check_if_game_ends(board):
+                winner = "black"
+                break
+            elif board.is_check():
+                print("Check")
         
-#         if user_color == "black":
-#             opening_moves(board, game_board, engine_list, current_opening, move_list, engine)
-#             if check_if_game_ends(board):
-#                 winner = "white"
-#                 break
-#             elif board.is_check():
-#                 print("Check")
+        if user_color == "black":
+            opening_moves(board, game_board, engine_list, current_opening, move_list, engine)
+            if check_if_game_ends(board):
+                winner = "white"
+                break
+            elif board.is_check():
+                print("Check")
             
-#             user_moves(board, game_board, move_list)
-#             if check_if_game_ends(board):
-#                 winner = "black"
-#                 break
-#             elif board.is_check():
-#                 print("Check")
+            user_moves(board, game_board, move_list)
+            if check_if_game_ends(board):
+                winner = "black"
+                break
+            elif board.is_check():
+                print("Check")
 
-#     engine.quit()
-#     display.terminate()
-#     # print(f"Game Over, Winner is {winner}") <--- fix this
+    engine.quit()
+    display.terminate()
+    # print(f"Game Over, Winner is {winner}") <--- fix this
 
-# play_chess_game()
+play_chess_game()
 
-board = chess.Board()
-board.push_san('Nf3')
-board.push('e4')
-board.push('e5')
-board.push('d4')
-board.push('d5')
+# board = chess.Board()
+# engine = chess.engine.SimpleEngine.popen_uci("stockfish")
+# result = engine.play(board, chess.engine.Limit(time=2.0))
+# board.push(result.move)
+# our_engine = ourEngine(board, "black", 3)
+# our_result = our_engine.search(board, 1, "black")
+# board.push(our_result)
 
-game_board = display.start(board.fen())
-# engine = ourEngine(board, "white", 3)
+# result = engine.play(board, chess.engine.Limit(time=2.0))
+# board.push(result.move)
+# our_engine = ourEngine(board, "black", 3)
+# our_result = our_engine.search(board, 1, "black")
+# board.push(our_result)
 
-# print(engine.search(board, 1, "white"))
+# result = engine.play(board, chess.engine.Limit(time=2.0))
+# board.push(result.move)
+# our_engine = ourEngine(board, "black", 3)
+# our_result = our_engine.search(board, 1, "black")
+# board.push(our_result)
+
+# result = engine.play(board, chess.engine.Limit(time=2.0))
+# board.push(result.move)
+# our_engine = ourEngine(board, "black", 3)
+# our_result = our_engine.search(board, 1, "black")
+# board.push(our_result)
+
+
+
+
+
+
+
+# game_board = display.start(board.fen())
+
+# sleep(10)
+
