9394d6cbf21381313254a5bb807a03639c65e561
Kira <kira.kaur.sidhu@gmail.com>
2023-08-06 16:52:39 -0700

    Added files

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler'
+import {gestureHandlerRootHOC} from 'react-native-gesture-handler'
+import Chessboard from 'react-native-chessboard';
 
 export default function App() {
-  return (
+  const ExampleWithHoc = gestureHandlerRootHOC(() => (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Chessboard
+        colors={ {black: 'pink', white: 'white'} }
+      />
     </View>
+  ));
+  return (
+    <ExampleWithHoc/>
   );
 }
 
