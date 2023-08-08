2baa486621435f91c8df89ea62d7f49cf8bc9cd5
Kira <kira.kaur.sidhu@gmail.com>
2023-08-08 12:07:19 -0700

    Consolidated engine call under post and patch, cleaned up code files

--- app/routes/game_routes.py
+++ app/routes/game_routes.py
@@ -3,56 +3,10 @@ import os
 from app.models.game import Game 
 from flask import Blueprint, make_response, request, jsonify, abort 
 import chess
-# temporary engine
-import chess.engine
-# Initalize chessboard display
-from chessboard import display
-from time import sleep
 from openings import opening_table
-from our_engine import ourEngine
+from engine_call import ChessGame
 
 game_bp = Blueprint("game_bp", __name__, url_prefix="/games") 
-
-# fix board.py for back and forth play
-class ChessGame:
-    def __init__(self, random=True):
-        self.random = random
-
-    def engine_moves(board, game_board, engine_list, engine):
-        engine = ourEngine(board, "white")
-        engine.evaluation("white")
-
-    def opening_moves(board, engine_list, current_opening, move_list, engine):
-        move_index = len(engine_list) + 1 
-        result = None 
-
-        for line in current_opening["variations"]: 
-            if not move_list: 
-                result = line[move_index][0]
-                board.push_san(result)
-                break
-            elif len(move_list) > 0: 
-                try:
-                    if line[move_index-1][1] == move_list[-1] and engine_list[-1] == line[move_index-1][0]:
-                        try:
-                            result = line[move_index][0]
-                            board.push_san(result)
-                            break
-                        except chess.IllegalMoveError:
-                            continue
-                    else: 
-                        continue
-                except IndexError:
-                    continue 
-
-        if not result: 
-            result = engine.search(board, 3, "white")
-            board.push(result)
-
-        new_engine_list = engine_list[:]
-        new_engine_list.append(result)
-        return board.fen(), new_engine_list
-        
 
 # validating function
 def validate_item(model, item_id):
@@ -63,42 +17,32 @@ def validate_item(model, item_id):
     
     return model.query.get_or_404(item_id)
 
-# nested helper function
-    # check if checkmate, game over, etc...
-def check_game_status(board):
-    if board.is_checkmate():
-        return "Checkmate"
-    if board.is_stalemate():
-        return "Stalemate"
-    if board.is_insufficient_material():
-        return "Insufficient Material"
-    if board.can_claim_draw():
-        return "Can Claim Draw"
-    if board.can_claim_threefold_repetition():
-        return "Can Claim Threefold Repetition"
-    if board.can_claim_fifty_moves():
-        return "Can Claim Fifty Moves"
-    if board.is_fivefold_repetition():
-        return "Is Fivefold Repetition"
-    if board.is_seventyfive_moves():
-        return "Completed 75 Moves"
-    return None
-
-
+# POST Route to start game
 @game_bp.route("", methods=["POST"])
 def start_game():
     request_body = request.get_json()
     new_board = chess.Board()
+    request_body["engine_move_list"] = []
+
+    if request_body["white"] == "engine":
+        new_game = ChessGame
+        for item in opening_table:
+            if request_body["opening"] == item["name"]:
+                current_opening = item
+
+        data = new_game.opening_moves(new_board, current_opening, [], [], "white")
+        request_body["engine_move_list"] = data[1]
 
     request_body["fen"] = new_board.fen()
 
-    new_game = Game.from_dict(request_body)
+    game = Game.from_dict(request_body)
 
-    db.session.add(new_game)
+    db.session.add(game)
     db.session.commit()
 
-    return {"id": int(new_game.game_id)}, 200
+    return {"id": int(game.game_id)}, 200
 
+# PATCH route to receive user move and send back engine move
 @game_bp.route("/<game_id>", methods=["PATCH"])
 def get_user_move(game_id):
     game = validate_item(Game, game_id)
@@ -106,12 +50,25 @@ def get_user_move(game_id):
     request_body = request.get_json()
 
     game.fen = request_body["fen"]
+    new_board = chess.Board(game.fen)
+    new_game = ChessGame
 
-    # current_status = check_game_status(game.board_init)
-    # if current_status:
-    #     game.game_status = current_status
+    current_status = new_game.check_game_status(new_board)
+    if current_status:
+        game.game_status = current_status
+    else:
+        if game.white == "engine":
+            engine_color = "white"
+        else:
+            engine_color = "black"
+        
+        for item in opening_table:
+            if game.opening == item["name"]:
+                current_opening = item
 
-    game.current_player = "engine"
+        data = new_game.opening_moves(new_board, current_opening, game.engine_move_list, game.user_move_list, engine_color)
+        game.fen = data[0]
+        game.engine_move_list = data[1]
 
     db.session.commit()
 
@@ -131,20 +88,6 @@ def get_games():
 @game_bp.route("/<game_id>", methods=["GET"])
 def get_engine_move(game_id):
     game = validate_item(Game, game_id)
-    current_board = chess.Board(game.fen)
-    continue_game = ChessGame
-    # current_status = check_game_status(current_board)
-    # if current_status:
-    #     game.game_status = current_status
-
-    for item in opening_table:
-        if game.opening == item["name"]:
-            current_opening = item
-
-    data = continue_game.opening_moves(current_board, game.engine_move_list, current_opening, game.user_move_list, ourEngine)
-    game.fen = data[0]
-    game.engine_move_list = data[1]
-    game.current_player = "player"
 
     response = game.to_dict() 
 
