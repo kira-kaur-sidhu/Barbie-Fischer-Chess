5bfe565ed78a71ed434c20e16d217d4073990e9d
Kimberly Lantigua <klantigua11@gmail.com>
2023-08-14 22:17:18 -0700

    Added button styling to Opening Page, consistent with Game page. Added functionality of changing opening options based on user player input. Experimented with the confirmMove button. Could not check axios check because Render is down.

--- components/Opening.js
+++ components/Opening.js
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
+// import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
 import {GestureHandlerRootView, gestureHandlerRootHOC} from 'react-native-gesture-handler';
 import Chessboard from 'react-native-chessboard';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Center, Box, Button, Flex, Heading } from 'native-base';
+import { useWindowDimensions } from 'react-native';
 
 /* TO DOs:
 1. update buttons to match GAME.js
@@ -28,6 +28,8 @@ const Opening = ({ route, navigation }) => {
     const [moveList, updateMoveList] = useState();
     const whitePlayer = route.color === 'white' ? 'user' : 'engine'; 
     const opening = route.opening;
+    const {height, width} = useWindowDimensions();
+    let confirmedMove = false;
     
     useEffect(() => {
         console.log(whitePlayer);
@@ -60,7 +62,6 @@ const Opening = ({ route, navigation }) => {
             console.log("We're inside the axios patch call")
             console.log(moveList)
             updateFen(result.data.game.fen);
-            // const newFen = fen
         })
         .catch((err) => {
             console.log(err); 
@@ -71,69 +72,99 @@ const Opening = ({ route, navigation }) => {
             <Chessboard
                 colors={ {black: '#F3BAD5', white: '#FFFBFB'} }
                 fen={ currentFen } 
-                onMove={({ state }) => {getEngineMove(state)
-                console.log(state)}}
+                onMove={({ state }) => {
+                    if (confirmedMove === true) {
+                        getEngineMove(state);
+                        confirmedMove = false;
+                        console.log(state);
+                        }
+                    }
+                }
             />
         )
     );
-
     return (
         <GestureHandlerRootView>
-            <View style={styles.container}>
-                <View style={styles.chessWrapTop}>
-                    <Text>Barbie</Text>
-                </View>
-                <ChessBoardRender />
-                <View style={styles.chessWrapBottom}>
-                    <Text>You</Text>
-                </View>
-                <View style={styles.infoButtons}>
-                    <View></View>
-                    <View style={styles.buttons}>
-                        <Button 
-                        title="Confirm"/>
-                        <View style={styles.space} />
-                        <Button
-                        title="Undo"/>
-                        <View style={styles.space} />
-                        <Button
-                        title="Resign"/>
-                    </View>
-                </View>
-            </View>
+            <Center>
+                <Flex direction="column" align="center" justify="space-between" h="100%" w="100%" safeAreaBottom>
+                    <Box w="100%">
+                    <Box bg={"#F3BAD5"} h={Math.floor(width / 8) * 1.5} w="100%" _text={{fontSize: 'md', fontWeight: 'bold'}}>Barbie</Box>
+                    <Box w={Math.floor(width / 8) * 8} h={Math.floor(width / 8) * 8}>
+                    <ChessBoardRender/>
+                    </Box>
+                    <Box bg={"#F3BAD5"} paddingTop={Math.floor(width / 8) * 1} w="100%" _text={{textAlign: 'right', fontSize: 'md', fontWeight: 'bold'}}>Player</Box>
+                    </Box>
+                    <Button.Group space={4} paddingBottom={10}>
+                        <Button>Undo</Button>
+                        <Button onPress={() => {confirmedMove = true}}>Confirm</Button>
+                    </Button.Group>
+                </Flex>
+            </Center>
         </GestureHandlerRootView>
     )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 10,
-    },
-    chessBox: {
-    },
-    chessWrapTop: {
-        backgroundColor: '#F3BAD5',
-        height: 50,
-        width: '100%',
-    },
-    chessWrapBottom: {
-        backgroundColor: '#F3BAD5',
-        height: 50,
-        width: '100%',
-        marginTop: 405,
-    },
-    buttons: {
-        padding: 10,
-        margin: 20,
-    },
-    space: {
-        width: 20,
-        height: 20,
-    },
-});
-
-
 export default Opening;
+
+
+
+
+
+
+// return (
+//     <GestureHandlerRootView>
+//         <View style={styles.container}>
+//             <View style={styles.chessWrapTop}>
+//                 <Text>Barbie</Text>
+//             </View>
+//             <ChessBoardRender />
+//             <View style={styles.chessWrapBottom}>
+//                 <Text>You</Text>
+//             </View>
+//             <View style={styles.infoButtons}>
+//                 <View></View>
+//                 <View style={styles.buttons}>
+//                     <Button 
+//                     title="Confirm"/>
+//                     <View style={styles.space} />
+//                     <Button
+//                     title="Undo"/>
+//                     <View style={styles.space} />
+//                     <Button
+//                     title="Resign"/>
+//                 </View>
+//             </View>
+//         </View>
+//     </GestureHandlerRootView>
+// )
+// }
+
+// const styles = StyleSheet.create({
+// container: {
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     marginTop: 10,
+// },
+// chessBox: {
+// },
+// chessWrapTop: {
+//     backgroundColor: '#F3BAD5',
+//     height: 50,
+//     width: '100%',
+// },
+// chessWrapBottom: {
+//     backgroundColor: '#F3BAD5',
+//     height: 50,
+//     width: '100%',
+//     marginTop: 405,
+// },
+// buttons: {
+//     padding: 10,
+//     margin: 20,
+// },
+// space: {
+//     width: 20,
+//     height: 20,
+// },
+// });
