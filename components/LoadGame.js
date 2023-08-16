16e10a8fe44912e68493c0a053a7d070bf139bb6
jasmine <jtmsoto@gmail.com>
2023-08-16 01:09:06 -0700

    Add styling

--- components/LoadGame.js
+++ components/LoadGame.js
@@ -1,8 +1,9 @@
 import * as React from 'react';
+import axios from 'axios';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button, useTheme, Image, AlertDialog, Spacer, Flex, HStack, Center, Heading, Box, FlatList } from "native-base";
 import 'react-native-gesture-handler';
@@ -16,47 +17,36 @@ but if you do want to work on it:
 3. create button to resume game (???? idk how to do that tho ???) */
 
 const LoadGame = () => {
-        const data = [{
-          id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-          fullName: "Aafreen Khan",
-          timeStamp: "12:47 PM",
-          recentText: "Good Day!",
-          avatarUrl: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-        }, {
-          id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-          fullName: "Sujitha Mathur",
-          timeStamp: "11:11 PM",
-          recentText: "Cheer up, there!",
-          avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyEaZqT3fHeNrPGcnjLLX1v_W4mvBlgpwxnA&usqp=CAU"
-        }, {
-          id: "58694a0f-3da1-471f-bd96-145571e29d72",
-          fullName: "Anci Barroco",
-          timeStamp: "6:22 PM",
-          recentText: "Good Day!",
-          avatarUrl: "https://miro.medium.com/max/1400/0*0fClPmIScV5pTLoE.jpg"
-        }, {
-          id: "68694a0f-3da1-431f-bd56-142371e29d72",
-          fullName: "Aniket Kumar",
-          timeStamp: "8:56 PM",
-          recentText: "All the best",
-          avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr01zI37DYuR8bMV5exWQBSw28C1v_71CAh8d7GP1mplcmTgQA6Q66Oo--QedAN1B4E1k&usqp=CAU"
-        }, {
-          id: "28694a0f-3da1-471f-bd96-142456e29d72",
-          fullName: "Kiara",
-          timeStamp: "12:47 PM",
-          recentText: "I will call today.",
-          avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBwgu1A5zgPSvfE83nurkuzNEoXs9DMNr8Ww&usqp=CAU"
-        }];
-        const [isOpen, setIsOpen] = React.useState(false);
-        const onClose = () => setIsOpen(false);
+    const [savedGames, setSavedGames] = useState([]);
+    const API = 'https://barbie-fischer-chess.onrender.com/games'
+    const [isOpen, setIsOpen] = React.useState(false);
+    const onClose = () => setIsOpen(false);
 
-        return <Box safeArea>
-            <Heading fontSize="xl" p="4" pb="3">
-              All Saved Games
-            </Heading>
-            <FlatList data={data} renderItem={({
-            item
-          }) => <Box borderBottomWidth="1" _dark={{
+    // useEffect(() => {
+    //   console.log('im inside useEffect');
+    //   axios.get(`${URL}`)
+    //   .then((result) => {
+    //     console.log('im inside the GET request')
+    //     const newGames = result.data.map((game) => {
+    //       return {
+    //         id: game.game_id,
+    //         fen: game.fen,
+    //         opening: game.opening
+    //       };
+    //     });
+    //     setSavedGames(newGames);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   })
+    // }, []);
+
+    return (
+      <Box safeArea>
+        <Heading fontSize="xl" p="4" pb="3">
+          All Saved Games
+        </Heading>
+        <FlatList data={savedGames} renderItem={({item}) => <Box borderBottomWidth="1" _dark={{
             borderColor: "muted.50"
           }} borderColor="muted.800" safeArea>
             <Flex direction='row' align='center' justify='space-between'>
@@ -65,7 +55,7 @@ const LoadGame = () => {
                       <Text _dark={{
                   color: "warmGray.50"
                 }} color="coolGray.800" bold>
-                        Saved Game {item.timeStamp}
+                        Saved Game {item.id}
                       </Text>
                       </HStack>
                     <Spacer />
@@ -96,7 +86,7 @@ const LoadGame = () => {
             </AlertDialog.Content>
           </AlertDialog>
         </Center>
-          </Box>;
+          </Box>)
 };
 
 export default LoadGame;
